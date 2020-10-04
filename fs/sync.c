99133523dd17a31099101eb57134048e28814ac0
Jimbo77 <chupyalcohol@gmail.com>
2020-10-04 12:55:59 -0600

    Added fsync on/off support.

--- fs/sync.c
+++ fs/sync.c
@@ -8,6 +8,7 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/export.h>
+#include <linux/module.h>
 #include <linux/namei.h>
 #include <linux/sched/xacct.h>
 #include <linux/writeback.h>
@@ -18,6 +19,9 @@
 #include <linux/backing-dev.h>
 #include <linux/version.h>
 #include "internal.h"
+
+bool fsync_enabled = true;
+module_param(fsync_enabled, bool, 0755);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -424,6 +428,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	struct fd f = fdget(fd);
 	struct super_block *sb;
 	int ret;
+	
+	if (!fsync_enabled)
+		return 0;
 
 	if (!f.file)
 		return -EBADF;
@@ -451,6 +458,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct inode *inode = file->f_mapping->host;
+	
+	if (!fsync_enabled)
+		return 0;
 
 	if (!file->f_op->fsync)
 		return -EINVAL;
@@ -470,6 +480,8 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
+	if (!fsync_enabled)
+		return 0;
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -500,6 +512,9 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct fd f = fdget(fd);
 	int ret = -EBADF;
+	
+	if (!fsync_enabled)
+		return 0;
 	unsigned long stamp = jiffies;
 
 	if (f.file) {
@@ -513,11 +528,15 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
+	if (!fsync_enabled)
+		return 0;
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
+	if (!fsync_enabled)
+		return 0;
 	return do_fsync(fd, 1);
 }
 
@@ -576,6 +595,9 @@ int ksys_sync_file_range(int fd, loff_t offset, loff_t nbytes,
 	struct address_space *mapping;
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
+	
+	if (!fsync_enabled)
+		return 0;
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
