a0c4274b45aebccf2add6d36bd3a3e4d0e3ecb6d
Jimbo77 <chupyalcohol@gmail.com>
2020-10-26 23:56:34 -0600

    UPSTREAM: tcp: add tcp_wstamp_ns socket field
    TCP will soon provide earliest departure time on TX skbs.
    It needs to track this in a new variable.
    
    tcp_mstamp_refresh() needs to update this variable, and
    became too big to stay an inline.

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -312,6 +312,8 @@ struct tcp_sock {
 		is_cwnd_limited:1,/* forward progress limited by snd_cwnd? */
 		syn_smc:1;	/* SYN includes SMC */
 	u32	tlp_high_seq;	/* snd_nxt at the time of TLP retransmit. */
+	
+	u64	tcp_wstamp_ns;	/* departure time for next sent data packet */
 
 /* RTT measurement */
 	u64	tcp_mstamp;	/* most recent packet received/sent */
