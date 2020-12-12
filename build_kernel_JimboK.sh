d7ae68bc6828eb35863350eba01173fd1b919031
Jimbo77 <chupyalcohol@gmail.com>
2020-12-11 22:18:21 -0600

    moved to compressed kernel image (gz)

--- build_kernel_JimboK.sh
+++ build_kernel_JimboK.sh
@@ -10,7 +10,7 @@ ccache -M 4.5
 
 export ARCH=arm64
 
-#make mrproper
+make mrproper
 
 mkdir out
 
@@ -18,7 +18,7 @@ DTS_DIR=$(pwd)/out/arch/$ARCH/boot/dts
 BUILD_CROSS_COMPILE=$(pwd)/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 #KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/8.0/bin/clang
 #KERNEL_LLVM_BIN=../toolchains/llvm/bin/clang
-KERNEL_LLVM_BIN=clang
+KERNEL_LLVM_BIN=$(pwd)/toolchains/llvm-arm-toolchain-ship-10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y VARIANT_DEFCONFIG=vendor/$1/kona_sec_$1_usa_singlew_defconfig"
@@ -36,7 +36,8 @@ make O=$(pwd)/out $KERNEL_MAKE_ENV CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$K
 
 make -j$(nproc) O=$(pwd)/out $KERNEL_MAKE_ENV CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CFP_CC=$KERNEL_LLVM_BIN
 
-cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
+cp $(pwd)/out/arch/$ARCH/boot/Image.gz $(pwd)/out/Image.gz
+cp $(pwd)/out/arch/$ARCH/boot/Image.gz-dtb $(pwd)/out/Image.gz-dtb
 
 DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo)
 
@@ -44,10 +45,11 @@ cat ${DTS_DIR}/vendor/qcom/*.dtb > $(pwd)/out/dtb.img
 
 $(pwd)/tools/mkdtimg create $(pwd)/out/dtbo.img --page_size=4096 ${DTBO_FILES}
 
-mv $(pwd)/out/Image $(pwd)/out/Image-JimboK_$1
+mv $(pwd)/out/Image.gz $(pwd)/out/Image-JimboK_$1.gz
 mv $(pwd)/out/dtb.img $(pwd)/out/dtb-JimboK_$1.img
 
-cp $(pwd)/out/Image-JimboK_$1 ~/build/mkbootimg-R
+cp $(pwd)/out/Image-JimboK_$1.gz ~/build/mkbootimg-R
 cp $(pwd)/out/dtb-JimboK_$1.img ~/build/mkbootimg-R
+cp $(pwd)/out/Image.gz-dtb ~/build/mkbootimg-R
 
 ~/build/mkbootimg-R/make_boot_$1.sh $2-$1
