e23b06e4bb1446020679172a87224efd738b11da
Jimbo77 <chupyalcohol@gmail.com>
2020-12-15 21:38:32 -0600

    Revert " msm-poweroff: keep console-ramoops alive"
    This reverts commit 6cfec0f54906d16d20c25958a40047fb3154d663.

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -520,8 +520,7 @@ static void msm_restart_prepare(const char *cmd)
 	if (force_warm_reboot)
 		pr_info("Forcing a warm reset of the system\n");
 #else
-	/* keep console-ramoops alive */
-	need_warm_reset = true
+	need_warm_reset = get_dload_mode();
 #endif
 
 	/* Hard reset the PMIC unless memory contents must be maintained. */
