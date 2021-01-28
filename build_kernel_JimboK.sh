35a5000e114ad73c93f60519d2143a9ee9ea49f6
Jimbo77 <chupyalcohol@gmail.com>
2021-01-28 10:36:25 -0600

    build: Updated build script, moving to KOR configs

--- build_kernel_JimboK.sh
+++ build_kernel_JimboK.sh
@@ -20,7 +20,7 @@ KERNEL_LLVM_BIN=$(pwd)/toolchains/llvm-arm-toolchain-ship-10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 BUILD_CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 
-KERNEL_MAKE_ENV="ARCH=arm64 DTC_EXT=$(pwd)/tools/dtc VARIANT_DEFCONFIG=vendor/$1/kona_sec_$1_usa_singlew_defconfig"
+KERNEL_MAKE_ENV="ARCH=arm64 DTC_EXT=$(pwd)/tools/dtc VARIANT_DEFCONFIG=vendor/$1/kona_sec_$1_kor_singlex_defconfig"
 
 make O=$(pwd)/out $KERNEL_MAKE_ENV CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE vendor/JimboK_defconfig
 
@@ -41,5 +41,6 @@ mv $(pwd)/out/dtb.img $(pwd)/out/dtb-JimboK_$1.img
 cp $(pwd)/out/Image-JimboK_$1.gz ~/build/mkbootimg-R
 cp $(pwd)/out/dtb-JimboK_$1.img ~/build/mkbootimg-R
 cp $(pwd)/out/Image.gz-dtb ~/build/mkbootimg-R
+cp $(pwd)/out/dtbo.img ~/build/mkbootimg-R
 
 ~/build/mkbootimg-R/make_boot_$1.sh $2-$1
